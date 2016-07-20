b506329eff72eb2d9ce532ca9c1ebb9ae0b0333b
Francois Belanger <frank@smartbuildingapps.com>
2016-07-19 23:24:59 -0400

    added first few specs to user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = FactoryGirl.build(:user) }
+  subject { @user }
+
+  it { is_expected.to respond_to(:email) }
+  it { is_expected.to respond_to(:password) }
+  it { is_expected.to respond_to(:password_confirmation) }
+  it { is_expected.to be_valid }
 end
