772ec375d25fd57fdea4e292b31d0c4923f72fd3
Francois Belanger <frank@smartbuildingapps.com>
2016-07-20 01:23:55 -0400

    added api/v1/user#create action and specs

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     scope :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: [:show], controller: 'v1/users'
+      resources :users, only: [:show, :create], controller: 'v1/users'
     end
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -9,12 +9,48 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       get :show, id: @user.id, format: :json
     end
 
-    it "should return the user as a JSON hash" do
+    it "should return the user as JSON" do
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
     end
 
     it { is_expected.to respond_with 200 }
+  end
+
+  describe "POST #create" do
+    context "when created" do
+
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for :user
+        post :create, { user: @user_attributes }, format: :json
+      end
+
+      it "should return the user created as JSON" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it { is_expected.to respond_with 201 }
+    end
+
+    context "when not created" do
+      before(:each) do
+        @invalid_attributes = FactoryGirl.attributes_for :user, email: nil
+        post :create, { user: @invalid_attributes }, format: :json
+      end
+
+      it "should render an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "should explain why the user could not be created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { is_expected.to respond_with 422 }
+    end
   end
 
 end
