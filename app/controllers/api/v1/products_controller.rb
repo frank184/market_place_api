fe4d607c914b3634b40b77ed97e84a6462749a08
Francois Belanger <frank@smartbuildingapps.com>
2016-07-21 23:14:15 -0400

    adds search class method to filter products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -3,12 +3,7 @@ class Api::V1::ProductsController < ApplicationController
   respond_to :json
 
   def index
-    products = if params[:product_ids].present?
-      Product.where(id: params[:product_ids]).includes(:user)
-    else
-      Product.all.includes(:user)
-    end
-    respond_with products
+    respond_with Product.search(params)
   end
 
   def show
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -30,4 +30,123 @@ RSpec.describe Product, type: :model do
     it { is_expected.to validate_numericality_of(:price).is_greater_than_or_equal_to(0) }
     it { is_expected.to validate_presence_of(:user) }
   end
+
+  describe "scopes" do
+    before(:each) { @user = create :user }
+
+    describe "#by_title" do
+      before(:each) do
+        @product1 = create :product, title: 'Plasma TV', user: @user
+        @product2 = create :product, title: 'Fastest Laptop', user: @user
+        @product3 = create :product, title: 'iPhone', user: @user
+        @product4 = create :product, title: 'LCD TV', user: @user
+      end
+
+      context "when pattern = 'TV'" do
+        before(:each) { @matches = Product.by_title 'TV' }
+
+        it "should return 2 items" do
+          expect(@matches).to have(2).items
+        end
+
+        it "should return the matching products" do
+          expect(@matches).to match_array([@product1, @product4])
+        end
+      end
+    end
+
+    describe "#greater_than_or_equal_price" do
+      before(:each) do
+        @product1 = create :product, price: 5, user: @user
+        @product2 = create :product, price: 10, user: @user
+        @product3 = create :product, price: 15, user: @user
+        @product4 = create :product, price: 20, user: @user
+      end
+
+      context "when pattern = 10" do
+        before(:each) { @matches = Product.greater_than_or_equal_price 15 }
+
+        it "should return 2 items" do
+          expect(@matches).to have(2).items
+        end
+
+        it "should return the matching products" do
+          expect(@matches).to match_array([@product3, @product4])
+        end
+      end
+    end
+
+    describe "#less_than_or_equal_price" do
+      before(:each) do
+        @product1 = create :product, price: 5, user: @user
+        @product2 = create :product, price: 10, user: @user
+        @product3 = create :product, price: 15, user: @user
+        @product4 = create :product, price: 20, user: @user
+      end
+
+      context "when pattern = 10" do
+        before(:each) { @matches = Product.less_than_or_equal_price 10 }
+
+        it "should return 2 items" do
+          expect(@matches).to have(2).items
+        end
+
+        it "should return the matching products" do
+          expect(@matches).to match_array([@product1, @product2])
+        end
+      end
+    end
+
+    describe "#latest" do
+      before(:each) do
+        @product1 = create :product, user: @user
+        @product2 = create :product, user: @user
+        @product3 = create :product, user: @user
+        @product4 = create :product, user: @user
+        # Update products
+        @product4.touch
+        @product3.touch
+        # Matches
+        @matches = Product.latest
+      end
+
+      it "should return the latest products" do
+        expect(@matches).to match_array([@product3, @product4, @product2, @product1])
+      end
+    end
+
+    describe "#search" do
+      before(:each) do
+        @product1 = create :product, user: @user, price: 100, title: "Plasma tv"
+        @product2 = create :product, user: @user, price: 50, title: "Videogame console"
+        @product3 = create :product, user: @user, price: 150, title: "MP3"
+        @product4 = create :product, user: @user, price: 99, title: "Laptop"
+      end
+
+      context "when title = 'Videogame' and min price = '100'" do
+        it "returns an empty array" do
+          expect(Product.search({ title: "videogame", min_price: 100 })).to be_empty
+        end
+      end
+
+      context "when title = 'TV' and max price = '50'" do
+        it "returns the product1" do
+          expect(Product.search({ title: "TV", max_price: 150 })).to match_array([@product1])
+        end
+      end
+
+      context "when an empty hash is sent" do
+        it "returns all the products" do
+          expect(Product.search({})).to match_array([@product1, @product2, @product3, @product4])
+        end
+      end
+
+      context "when product_ids is present" do
+        it "returns the product from the ids" do
+          expect(Product.search({ product_ids: [@product1.id, @product2.id] })).to match_array([@product1, @product2])
+        end
+      end
+    end
+  end
+
 end
