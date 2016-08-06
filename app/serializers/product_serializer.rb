c518b1cd962897bcdc95eed9431c4fece38791c4
Francois Belanger <frank@smartbuildingapps.com>
2016-08-06 19:30:19 -0400

    cached products serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,10 @@
 class ProductSerializer < ActiveModel::Serializer
   attributes :id, :title, :price, :published
   belongs_to :user, include: true
+
+  cache
+
+  def cache_key
+    [object, scope]
+  end
 end
